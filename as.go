2bd9a84a6534c48ce54429eee2e257bde06b4d78
greg-sk <grzegorz.luczywo@securitykiss.com>
2016-06-06 13:06:02 +0200

    split functions

--- as.go
+++ as.go
@@ -1,5 +1,11 @@
 package as
 
+import "regexp"
+
+// eias - everything is a string
+
+// operations on slice of strings
+
 func Dummy() {
 }
 
@@ -18,4 +24,79 @@ func Map(ss []string, f func(string) (string, error)) ([]string, error) {
 func MustMap(ss []string, f func(string) string) []string {
 	result, _ := Map(ss, func(s string) (string, error) { return f(s), nil })
 	return result
+}
+
+func IndexOf(ss []string, elem string) int {
+	for i, s := range ss {
+		if s == elem {
+			return i
+		}
+	}
+	return -1
+}
+
+func IndexOfPattern(ss []string, pattern string) int {
+	re := regexp.MustCompile(pattern)
+	for i, s := range ss {
+		if re.MatchString(s) {
+			return i
+		}
+	}
+	return -1
+}
+
+func IndicesOf(ss []string, elem string) []int {
+	var result = make([]int, 0)
+	for i, s := range ss {
+		if s == elem {
+			result = append(result, i)
+		}
+	}
+	return result
+}
+
+func IndicesOfPattern(ss []string, pattern string) []int {
+	re := regexp.MustCompile(pattern)
+	var result = make([]int, 0)
+	for i, s := range ss {
+		if re.MatchString(s) {
+			result = append(result, i)
+		}
+	}
+	return result
+}
+
+// split slice of strings into multiple slices of strings
+// splitpoints provide the line numbers at which to split
+// return (len(splitpoints) + 1) slices/sections of slices of strings
+// if splitpoints is empty return the orginal ss as a single element of external slice
+func SplitAt(ss []string, splitpoints []int) [][]string {
+	result := make([][]string, len(splitpoints)+1)
+	prevP := 0
+	for i, p := range splitpoints {
+		result[i] = ss[prevP:p]
+		prevP = p
+	}
+	result[len(splitpoints)] = ss[prevP:]
+	return result
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// This is actually for imath package
+//////////////////////////////////////////////////////////////////////////////
+
+func Max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func Min(a int, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
 }
--- as_test.go
+++ as_test.go
@@ -5,12 +5,52 @@ import (
 	"testing"
 )
 
+func slice1() []string {
+	return []string{"one", "two", "three", "one", "two"}
+}
+
 func TestMustMap(t *testing.T) {
-	in := []string{"a", "bb", "ccc"}
-	want := []string{`"a"`, `"bb"`, `"ccc"`}
+	in := slice1()
+	want := []string{`"one"`, `"two"`, `"three"`, `"one"`, `"two"`}
 	quote := func(s string) string { return `"` + s + `"` }
 	got := MustMap(in, quote)
 	if !reflect.DeepEqual(got, want) {
-		t.Errorf("MustMap(%s) == %s, want %s", in, got, want)
+		t.Errorf("MustMap(%v) == %v, want %v", in, got, want)
+	}
+}
+
+func TestIndicesOf(t *testing.T) {
+	in := slice1()
+	want := []int{1, 4}
+	got := IndicesOf(in, "two")
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("IndicesOf(%v, two) == %v, want %v", in, got, want)
+	}
+}
+
+func TestIndicesOfPattern(t *testing.T) {
+	in := slice1()
+	want := []int{1, 2, 4}
+	got := IndicesOfPattern(in, "^t")
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("IndicesOfPattern(%v, two) == %v, want %v", in, got, want)
+	}
+}
+
+func TestIndicesOfPattern2(t *testing.T) {
+	in := slice1()
+	want := []int{0, 2, 3}
+	got := IndicesOfPattern(in, "^on|^th")
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("IndicesOfPattern2(%v) == %v, want %v", in, got, want)
+	}
+}
+
+func TestSplitAt(t *testing.T) {
+	in := slice1()
+	want := [][]string{[]string{"one", "two"}, []string{"three", "one"}, []string{"two"}}
+	got := SplitAt(in, []int{2, 4})
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("SplitAt(%+v) = %+v, want %+v", in, got, want)
 	}
 }
